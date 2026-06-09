9292b596d8118b8cce19f74544d07effcce00be3
SavelevMatthew <savelevmatthew@gmail.com>
2026-06-09 13:01:27 +0500

    feat: implement CondoWebAppGetAvailableMethods

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,16 +1,54 @@
-import type { AnyRequestMethodName, RequestParams, ResultResponseDataMap } from '@open-condo/bridge'
+import type {
+	AnyRequestMethodName,
+	RequestParams,
+	ResultResponseDataMap,
+	ErrorReason,
+	ErrorCode,
+	ErrorResponseData,
+	GetAvailableMethodsParams,
+	GetAvailableMethodsData,
+} from '@open-condo/bridge'
 
 export class BridgeCordovaAdapter {
-	#supportedMethods: Array<AnyRequestMethodName> = []
+	readonly #registeredHandlers: Partial<{
+		[T in AnyRequestMethodName]: (params: RequestParams<T>) => Promise<ResultResponseDataMap[T]>
+	}> = {}
+
+	constructor() {
+		this.#registeredHandlers = {
+			CondoWebAppGetAvailableMethods: this.#getAvailableMethods.bind(this),
+		}
+	}
+
+	async #getAvailableMethods(_params: GetAvailableMethodsParams): Promise<GetAvailableMethodsData> {
+		return {
+			methods: Object.keys(this.#registeredHandlers) as Array<AnyRequestMethodName>,
+		}
+	}
+
+	#getError<Reason extends ErrorReason>(reason: Reason, code: ErrorCode<Reason>, message: string): ErrorResponseData {
+		return {
+			errorType: 'client',
+			errorCode: code,
+			errorReason: reason,
+			errorMessage: message,
+		}
+	}
 
 	async execute<Method extends AnyRequestMethodName>(
 		method: Method,
-		_params: RequestParams<Method>,
+		params: RequestParams<Method>,
 	): Promise<ResultResponseDataMap[Method]> {
-		if (!this.#supportedMethods.includes(method)) {
-			// t
+		const handler = this.#registeredHandlers[method]
+		if (!handler) {
+			throw this.#getError('UNKNOWN_METHOD', 2, 'Not supported')
 		}
 
-		throw new Error('Not implemented')
+		try {
+			return await handler(params)
+		} catch (error) {
+			const msg = error instanceof Error ? error.message : String(error)
+			throw this.#getError('HANDLER_ERROR', 4, msg)
+		}
 	}
 }
